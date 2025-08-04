5776a465fa0b5ef1ace8ca9375d430b3e318e6e9
Jefferson <ajeffersonsouza95@gmail.com>
2025-08-04 16:59:38 -0300

    it simplifies the validation for phone number

--- clinic.domain/ValueObject/PhoneNumber.cs
+++ clinic.domain/ValueObject/PhoneNumber.cs
@@ -5,6 +5,7 @@ namespace clinic.domain.ValueObject
     public class PhoneNumber
     {
         [Required(ErrorMessage = "This field is required.")]
+        [RegularExpression("^\\(?[1-9]{2}\\)? ?(?:[2-8]|9[1-9])[0-9]{3,4}[- ]?[0-9]{4}$", ErrorMessage = "Wrong format.")]
         public string Number { get; set; }
     }
 }
